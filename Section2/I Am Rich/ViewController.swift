5bef5a414c59a0025968e3eb61d4a25085af3918
kiharayuta <k.kyuta.821@gmail.com>
2019-11-20 10:35:03 -0800

    Xcode Storyboards and Interface Builder

---
+++ Section2/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Yuta Kihara on 2019/11/20.
+//  Copyright © 2019 Yuta Kihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
